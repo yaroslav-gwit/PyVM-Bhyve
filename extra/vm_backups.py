ef70d223f914b3b5e24499c65dc12662a5aae232
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-01-03 13:49:23 +0000

    Added support for limiting the number of snapshots

--- extra/vm_backups.py
+++ extra/vm_backups.py
@@ -49,13 +49,13 @@ class VmSnapshot():
             vm_zfs_snapshots_to_delete = vm_zfs_snapshot_list.copy()
             for zfs_snapshot in range(0, snapshots_to_keep):
                 del vm_zfs_snapshots_to_delete[-1]
-            
-            print(vm_zfs_snapshots_to_delete)
+            # Remove the old snapshots
+            for vm_zfs_snapshot_to_delete in vm_zfs_snapshots_to_delete:
+                command = "zfs destroy " + vm_zfs_snapshot_to_delete
+                print(command)
         else:
             command = "zfs snapshot " + zfs_dataset + "@" + snapshot_name
             subprocess.run(command, shell=True, stderr = subprocess.DEVNULL, stdout=subprocess.DEVNULL)
-        # if debug == True:
-            # print(command)
 
 class VmReplication():
     def __init__(self, vmname="None", endpoint="None"):
