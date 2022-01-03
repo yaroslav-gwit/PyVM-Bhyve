f677264d31db4324ed8bf1336954713d2418a7d2
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-01-03 17:18:28 +0000

    Added testing flag to VM config file

--- extra/vm_deploy.py
+++ extra/vm_deploy.py
@@ -9,6 +9,7 @@ import random
 from jinja2 import Template
 import ast
 from os import listdir
+import re
 
 # Own classes
 from vm_dns_registry import vm_dns_registry
@@ -638,7 +639,10 @@ class VmDeployment():
             contents = file_object.read()
 
         tm = Template(contents)
-        live_status = "Production"
+        if re.match("^test-vm-", self.new_vm_name): 
+            live_status = "Testing"
+        else:
+            live_status = "Production"
         msg_vmtemplate = tm.render(cpus="2", memory="1G", switch=self.host_internal_switch_name, mac_address=self.vm_macaddress, vnc_port=self.vm_vnc_port, vnc_password=random_vnc_password, ip_address=self.vm_ipaddress, os_type=self.vm_os_type, live_status=live_status)
 
         with open(write_to_file, 'w') as file_object:
