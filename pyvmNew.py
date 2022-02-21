fa286d3be11f8db0fc3841004026f362007086ff
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-02-21 00:33:03 +0000

    Test

--- pyvmNew.py
+++ pyvmNew.py
@@ -14,10 +14,20 @@ import subprocess
 """ Section below is responsible for the CLI input/output """
 app = typer.Typer(context_settings=dict(max_content_width=800))
 
+
 @app.command()
 def vmlist(json: bool = typer.Option(False, help="Output json instead of a table")):
     """
     Example: hoster vmlist
+    """
+
+    print("Hello from few pyvm!")
+
+
+@app.command()
+def vmdeploy(quick: bool = typer.Option(True, help="Clone existing template ZFS dataset instead of copying over VM image")):
+    """
+    Example: hoster vmdeploy
     """
 
     print("Hello from few pyvm!")
