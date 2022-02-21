624b158705728d6a652c50cc8c8486731cd30785
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-02-21 00:52:21 +0000

    Test

--- pyvmNew.py
+++ pyvmNew.py
@@ -10,27 +10,31 @@ import subprocess
 # from extra import vm_mass_operations
 # from extra import vm_deploy
 # from extra import vm_backups
+import cli.vm.list as vm_list_cli
+import cli.vm.deploy as vm_deploy_cli
 
 """ Section below is responsible for the CLI input/output """
 app = typer.Typer(context_settings=dict(max_content_width=800))
+app.add_typer(vm_list_cli.app, name="users")
+app.add_typer(vm_deploy_cli.app, name="items")
 
 
-@app.command()
-def vmlist(json: bool = typer.Option(False, help="Output json instead of a table")):
-    """
-    Example: hoster vmlist
-    """
+# @app.command()
+# def vmlist(json: bool = typer.Option(False, help="Output json instead of a table")):
+#     """
+#     Example: hoster vmlist
+#     """
 
-    print("Hello this is VM List!")
+#     print("Hello this is VM List!")
 
 
-@app.command()
-def vmdeploy(quick: bool = typer.Option(True, help="Clone existing template ZFS dataset instead of copying over VM image")):
-    """
-    Example: hoster vmdeploy
-    """
+# @app.command()
+# def vmdeploy(quick: bool = typer.Option(True, help="Clone existing template ZFS dataset instead of copying over VM image")):
+#     """
+#     Example: hoster vmdeploy
+#     """
 
-    print("Hello this is VM Deploy")
+#     print("Hello this is VM Deploy")
 
 
 
