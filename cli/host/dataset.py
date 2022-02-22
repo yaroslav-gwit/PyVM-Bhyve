002820ac4e3232df64bece449db3c78c25c68985
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-02-22 15:45:30 +0000

    Test

--- cli/host/dataset.py
+++ cli/host/dataset.py
@@ -1,12 +1,11 @@
 import json
 import os
 
-with open("./configs/datasets.json", "r") as file:
-    datasets_file = file.read()
-
 class DatasetList:
     def __init__(self):
+        with open("./configs/datasets.json", "r") as file:
+            datasets_file = file.read()
         datasets_file = json.loads(datasets_file)
-        datasets_file = json.dumps(datasets_file, indent=2)
+        # datasets_file = json.dumps(datasets_file, indent=2)
 
         self.datasets = datasets_file
