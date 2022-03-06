d71cda0e00ce698cb1549e59ba182abf64c8be6a
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-03-06 13:59:17 +0000

    Test

--- cli/vm/vm.py
+++ cli/vm/vm.py
@@ -222,8 +222,8 @@ class VmList:
                 command = "grep 'bhyve: " + vm_name + "' /tmp/bhyve_vms_uptime.txt | grep -v grep | awk '{print $1}'"
                 shell_command = subprocess.check_output(command, shell=True)
                 vm_uptime = shell_command.decode("utf-8").split()[0]
-                # vmColumnUptime.append(vm_uptime)
-                vmColumnUptime.append(os.path.getmtime("/tmp/bhyve_vms_uptime.txt"))
+                vmColumnUptime.append(vm_uptime)
+                # vmColumnUptime.append(os.path.getmtime("/tmp/bhyve_vms_uptime.txt"))
             else:
                 vmColumnUptime.append("-")
 
