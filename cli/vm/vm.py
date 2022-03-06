514ee8d809e17ca119090824a2fc1599919108b3
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-03-06 12:33:38 +0000

    Test

--- cli/vm/vm.py
+++ cli/vm/vm.py
@@ -214,7 +214,8 @@ class VmList:
         vmColumnUptime = []
         for vm_name in vmColumnNames:
             if CoreChecks(vm_name).vm_is_live():
-                if (datetime.datetime.now() + timedelta(seconds=10)) > time.ctime(os.path.getmtime("/tmp/bhyve_vms_uptime.txt")):
+                timestamp = time.mktime((datetime.datetime.now() + timedelta(seconds=10)).timetuple())
+                if timestamp > os.path.getmtime("/tmp/bhyve_vms_uptime.txt"):
                     command = "ps axwww -o etime,command > /tmp/bhyve_vms_uptime.txt"
                 subprocess.run(command, shell=True)
                 command = "grep 'bhyve: " + vm_name + "' /tmp/bhyve_vms_uptime.txt | grep -v grep | awk '{print $1}'"
