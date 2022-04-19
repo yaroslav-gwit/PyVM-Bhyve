2d6df0951703976a1daf9f9bf2d9e1f635604919
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-04-19 15:16:36 +0100

    Test

--- cli/vm/vm.py
+++ cli/vm/vm.py
@@ -426,6 +426,7 @@ def snapshot_all(stype:str = typer.Option("custom", help="Snapshot type: daily, 
     vm_list = VmList().json_output()
     for _vm in vm_list:
         print(_vm)
+        type(_vm)
         # Operation.snapshot(vm_name=_vm, keep=keep, stype=stype)
 
 
