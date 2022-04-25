0ac3753b9e5a054944fea837e671b2895e43dae5
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-04-25 21:03:39 +0100

    Test

--- cli/vm/vmdeploy.py
+++ cli/vm/vmdeploy.py
@@ -3,18 +3,8 @@ import typer
 app = typer.Typer()
 
 @app.command()
-def create(item: str):
-    typer.echo(f"Creating item: {item}")
-
-
-@app.command()
-def delete(item: str):
-    typer.echo(f"Deleting item: {item}")
-
-
-@app.command()
-def sell(item: str):
-    typer.echo(f"Selling item: {item}")
+def deploy(item: str):
+    print("Deploy Test")
 
 
 if __name__ == "__main__":
