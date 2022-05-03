b602d90d0f34011eaf11746eb10d2c80824adb39
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-05-03 13:19:33 +0100

    test

--- cli/vm/vm.py
+++ cli/vm/vm.py
@@ -381,7 +381,7 @@ class VmDeploy:
         
         for _vm in VmList().plainList:
             _vm_vnc_port = CoreChecks(vm_name=_vm).vm_vnc_port()
-            _vm_vnc_port = int(vnc_port)
+            _vm_vnc_port = int(_vm_vnc_port)
             existing_vnc_ports.append(_vm_vnc_port)
         
         if vnc_port not in allowed_vnc_ports:
