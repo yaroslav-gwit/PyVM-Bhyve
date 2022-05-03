b1982f8e0f5328e02830e4288e4378d0627154d4
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-05-03 23:45:43 +0100

    test

--- cli/vm/vm.py
+++ cli/vm/vm.py
@@ -1098,7 +1098,7 @@ def stop_all(wait:int = typer.Option(5, help="Seconds to wait before stopping ne
 @app.command()
 def deploy(vm_name:str = typer.Argument("test-vm", help="New VM name"),
         os_type:str = typer.Option("ubuntu2004", help="OS Type, for example: debian11 or ubuntu2004"),
-        ip_address:str = typer.Option("10.0.0.0", help="Specify the IP address or leave at default to generate a random address"),
+        # ip_address:str = typer.Option("10.0.0.0", help="Specify the IP address or leave at default to generate a random address"),
         ds_id:int = typer.Option(0, help="Dataset ID to which this VM will be deployed"),
         ):
         """
