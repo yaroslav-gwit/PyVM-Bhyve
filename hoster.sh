49df98d08f64496887c7a23d33d06f94f917fec2
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-05-12 13:51:01 +0100

    test

--- hoster.sh
+++ hoster.sh
@@ -5,6 +5,9 @@ source bin/activate
 if [[ -z "$1" ]]; then
     ./hoster host info
     ./hoster vm list
+elif [[ $1 == "init" ]]; then
+    ./hoster host init
+    ./hoster network init
 else
     ./hoster $@
 fi
