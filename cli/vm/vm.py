bfdf1311562988293c6de89d0429de6b66fe31d8
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-05-22 00:20:11 +0100

    test

--- cli/vm/vm.py
+++ cli/vm/vm.py
@@ -223,7 +223,7 @@ class VmList:
             if CoreChecks(vm_name).vm_is_live():
                 state = "🟢"
             elif vm_config.get("parent_host") != host.HostInfo().hostName:
-                state = "🔷"
+                state = "💾"
             else:
                 state = "🔴"
             if CoreChecks(vm_name).vm_is_encrypted():
