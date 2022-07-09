b6f26c19ea78e2aa94055c4eb0575260f5e27f52
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-07-10 00:06:20 +0100

    VM start shell helper improvemenets

--- cli/shell_helpers/vm_start.sh
+++ cli/shell_helpers/vm_start.sh
@@ -5,20 +5,21 @@ VM_NAME=$2
 echo ""
 echo "__NEW_START__"
 echo "Time and date: $(date)"
-echo "This bhyve command was executed:"
+
+echo ""
+echo "This bhyve command was executed to start the VM:"
 echo $COMMAND
 
 echo ""
-
-echo "VM name: $VM_NAME"
-echo ""
-
 $COMMAND
 
 while [[ $? == 0 ]]
 do
+    echo ""
+    echo "VM has been restarted at: $(date)"
     $COMMAND
     sleep 5
+    echo ""
 done
 
 sleep 3
