b88a7161989224cbf9793ba061bbe9bc5a6789f1
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-07-10 14:20:24 +0100

    CI

--- cli/shell_helpers/vm_start.sh
+++ cli/shell_helpers/vm_start.sh
@@ -1,8 +1,8 @@
 #!/usr/local/bin/bash
 
 func_stop() {
-    echo "Stopping CHILD_PROCESS: $!"
-    kill -s SIGTERM $!
+    echo "$!" | tee /var/run/${VM_NAME}.pid
+    # kill -s SIGTERM $!
 }
 
 func_kill() {
@@ -39,7 +39,7 @@ echo "This bhyve command was executed to start the VM:"
 echo $COMMAND
 
 echo ""
-$COMMAND & $(echo "$!" | tee /var/run/${VM_NAME}.pid)
+$COMMAND & func_stop
 
 # echo ""
 # PARENT_PID=$$
@@ -53,7 +53,7 @@ while [[ $? == 0 ]]
 do
     echo ""
     echo "VM has been restarted at: $(date)"
-    $COMMAND & $(echo "$!" | tee /var/run/${VM_NAME}.pid)
+    $COMMAND & func_stop
     sleep 1
     echo ""
 done
