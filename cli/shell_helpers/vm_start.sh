96b0df512ad4790e129c6ac8bee1182a388410a8
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-07-10 14:22:18 +0100

    CI

--- cli/shell_helpers/vm_start.sh
+++ cli/shell_helpers/vm_start.sh
@@ -39,7 +39,8 @@ echo "This bhyve command was executed to start the VM:"
 echo $COMMAND
 
 echo ""
-$COMMAND & func_stop
+$COMMAND &
+func_stop
 
 # echo ""
 # PARENT_PID=$$
@@ -53,7 +54,8 @@ while [[ $? == 0 ]]
 do
     echo ""
     echo "VM has been restarted at: $(date)"
-    $COMMAND & func_stop
+    $COMMAND &
+    func_stop
     sleep 1
     echo ""
 done
