737e8afe184bc69aa1b00133dae330f4b06f5cbe
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-07-19 10:33:59 +0100

    test

--- cli/vm/vm.py
+++ cli/vm/vm.py
@@ -1133,7 +1133,7 @@ class ZFSReplication:
                         zfs_receive_command = " zfs receive -F "
                     else:
                         zfs_receive_command = " zfs receive "
-                    command = "zfs send -i " + snapshot_value + " " + vm_zfs_snapshot_list[snapshot_index + 1] + " | pv -p -e -r -W -w 400 -s " + str(round(shell_output)) + " | ssh " + ep_address + zfs_receive_command + vm_dataset
+                    command = "zfs send -i " + snapshot_value + " " + vm_zfs_snapshot_list[snapshot_index + 1] + " | pv -p -e -r -W -t -s " + str(round(shell_output)) + " | ssh " + ep_address + zfs_receive_command + vm_dataset
                     # print(command)
                     subprocess.run(command, shell=True)
                     # with subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=2) as sp:
@@ -1147,7 +1147,7 @@ class ZFSReplication:
             shell_output = shell_output.decode("UTF-8").strip("\n").split()[-1]
             print(" 🔷 DEBUG: Starting the INITIAL replication operation for: '" + vm_dataset + "'" + " (size: " + shell_output + ")")
             shell_output = float(shell_output.strip("G")) * 1024 * 1024 * 1024
-            command = "zfs send " + vm_zfs_snapshot_list[0] + " | pv -p -e -r -W -w 400 -s " + str(round(shell_output)) + " | ssh " + ep_address + " zfs receive " + vm_dataset
+            command = "zfs send " + vm_zfs_snapshot_list[0] + " | pv -p -e -r -W -t -s " + str(round(shell_output)) + " | ssh " + ep_address + " zfs receive " + vm_dataset
             subprocess.run(command, shell=True)
             print(" 🟢 INFO: Initial snapshot replication operation: done sending '" + vm_dataset + "'")
 
