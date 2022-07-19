eca5a6f91b0e1744625e7d6abf1450e9b37627bf
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-07-19 09:20:10 +0100

    CI

--- cli/vm/vm.py
+++ cli/vm/vm.py
@@ -1123,17 +1123,15 @@ class ZFSReplication:
             print(" 🔷 DEBUG: Starting the replication operation for: '" + vm_dataset + "'")
             for snapshot_index, snapshot_value in enumerate(vm_zfs_snapshot_list):
                 if snapshot_index != len(vm_zfs_snapshot_list)-1:
-                    command = "zfs send -vi " + snapshot_value + " " + vm_zfs_snapshot_list[snapshot_index + 1] + " | ssh " + ep_address + " zfs receive " + vm_dataset
+                    # command = "zfs send -vi " + snapshot_value + " " + vm_zfs_snapshot_list[snapshot_index + 1] + " | ssh " + ep_address + " zfs receive " + vm_dataset
+                    command = "zfs send -nP " + snapshot_value + " " + vm_zfs_snapshot_list[snapshot_index + 1] + " | ssh " + ep_address + " zfs receive " + vm_dataset
                     print(" 🔷 DEBUG: Sending snapshot " + str(snapshot_index + 1) + " out of " + str(len(vm_zfs_snapshot_list)-1))
-                    
-                    # with subprocess.Popen(shlex(command), stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
-                    #     for line in p.stdout:
-                    #         print("Line from Python3! : " + line, end='')
-                    with subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=2) as sp:
-                        for line in sp.stdout:
-                            print("Python Line! ")
-                            print(line.decode("UTF-8").strip("\n"))
-                # subprocess.run(command, shell=True)
+                    shell_output = subprocess.check_output(command, shell=True)
+                    print(shell_output)
+                    # with subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=2) as sp:
+                    #     for line in sp.stdout:
+                    #         print("Python Line! ")
+                    #         print(line.decode("UTF-8").strip("\n"))
             print(" 🟢 INFO: Replication operation: done sending '" + vm_dataset + "'")
         else:
             print(" 🔷 DEBUG: Starting the INITIAL replication operation for: '" + vm_dataset + "'")
