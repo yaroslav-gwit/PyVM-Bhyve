10947e89e07b5b46220524149b1993746a68c657
Yaroslav Koisa <yaroslav@gateway-it.com>
2022-08-17 00:21:21 +0100

    test

--- cli/vm/vm.py
+++ cli/vm/vm.py
@@ -770,19 +770,16 @@ class Operation:
                     child_pid = psutil.Process(parent_pid).children()[-1].pid
                     running_vm_pid = str(child_pid)
                     command = "kill -s SIGKILL " + running_vm_pid
-                    print(command)
                     shell_command = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
 
                     print(" 🔶 INFO: Could not find the PID file for: " + vm_name)
                     if not quiet:
                         print(" 🔶 INFO: Could not find the PID file for: " + vm_name)
                     command = "top -b -d1 -a all | grep \"/" + vm_name + "/\" | grep bash | awk '{print $1}'"
-                    print(command)
                     shell_command = subprocess.check_output(command, shell=True)
                     console_list = shell_command.decode("utf-8").split()
                     command = "kill -s SIGKILL " + console_list[0]
                     subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-                    print(command)
                     if not quiet:
                         print(" 🔶 INFO: Forcefully killed the VM process: " + console_list[0] + " " + vm_name)
 
@@ -792,12 +789,10 @@ class Operation:
                     if not quiet:
                         print(" 🔶 INFO: Could not find the PID file for: " + vm_name)
                     command = "top -b -d1 -a all | grep \"/" + vm_name + "/\" | grep bash | awk '{print $1}'"
-                    print(command)
                     shell_command = subprocess.check_output(command, shell=True)
                     console_list = shell_command.decode("utf-8").split()
                     command = "kill -s SIGKILL " + console_list[0]
                     subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-                    print(command)
                     if not quiet:
                         print(" 🔶 INFO: Forcefully killed the VM process: " + console_list[0] + " " + vm_name)
             
@@ -806,12 +801,10 @@ class Operation:
                 if not quiet:
                     print(" 🔶 INFO: Could not find the PID file for: " + vm_name)
                 command = "top -b -d1 -a all | grep \"/" + vm_name + "/\" | grep bash | awk '{print $1}'"
-                print(command)
                 shell_command = subprocess.check_output(command, shell=True)
                 console_list = shell_command.decode("utf-8").split()
                 command = "kill -s SIGKILL " + console_list[0]
                 subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-                print(command)
                 if not quiet:
                     print(" 🔶 INFO: Forcefully killed the VM process: " + console_list[0] + " " + vm_name)
                 # print(e)
